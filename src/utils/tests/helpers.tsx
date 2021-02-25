1a6abce83b148fa7152f1e52b7d4cbb66721c0dd
glauciocorreia <glauciocorreiaa@gmail.com>
2021-02-25 19:25:59 -0300

    Logo 5 - Criando primeiros testes

--- src/utils/tests/helpers.tsx
+++ src/utils/tests/helpers.tsx
@@ -1,8 +1,8 @@
-import { Themeprovider } from 'styled-components'
+import { ThemeProvider } from 'styled-components'
 import { render, RenderResult } from '@testing-library/react'
 
 import theme from 'styles/theme'
 import React from 'react'
 
 export const renderWithTheme = (children: React.ReactNode): RenderResult =>
-  render(<Themeprovider theme={theme}>{children}</Themeprovider>)
+  render(<ThemeProvider theme={theme}>{children}</ThemeProvider>)
