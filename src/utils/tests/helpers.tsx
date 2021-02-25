f3d54529f29118f6429daec1e8df916941a1212d
glauciocorreia <glauciocorreiaa@gmail.com>
2021-02-25 18:57:37 -0300

    Logo 4 - Criando um helper de testes renderWithTheme

---
+++ src/utils/tests/helpers.tsx
@@ -0,0 +1,8 @@
+import { Themeprovider } from 'styled-components'
+import { render, RenderResult } from '@testing-library/react'
+
+import theme from 'styles/theme'
+import React from 'react'
+
+export const renderWithTheme = (children: React.ReactNode): RenderResult =>
+  render(<Themeprovider theme={theme}>{children}</Themeprovider>)
