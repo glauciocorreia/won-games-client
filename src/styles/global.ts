b4a66b689f79559fc9d0ebde131046ac325de5c5
glauciocorreia <glauciocorreiaa@gmail.com>
2021-03-18 23:56:18 -0300

    Home 3 - Removendo background global do Storybook

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,53 +1,64 @@
-import { createGlobalStyle, css } from 'styled-components'
+import {
+  createGlobalStyle,
+  css,
+  DefaultTheme,
+  GlobalStyleComponent,
+} from 'styled-components'
 
-const GlobalStyles = createGlobalStyle`
+type GlobalStylesProps = {
+  removeBg?: boolean
+}
 
+const GlobalStyles: GlobalStyleComponent<
+  GlobalStylesProps,
+  DefaultTheme
+> = createGlobalStyle`
   @font-face {
     font-family: 'Poppins';
     font-style: normal;
     font-weight: 300;
-    src: local(''),
-        url('/fonts/poppins-v15-latin-300.woff2') format('woff2');
+    font-display: swap;
+    src: local('Poppins Light'), local('Poppins-Light'),
+        url('/fonts/poppins-v12-latin-300.woff2') format('woff2');
   }
-
   @font-face {
     font-family: 'Poppins';
     font-style: normal;
     font-weight: 400;
-    src: local(''),
-        url('/fonts/poppins-v15-latin-regular.woff2') format('woff2');
+    font-display: swap;
+    src: local('Poppins Regular'), local('Poppins-Regular'),
+        url('/fonts/poppins-v12-latin-regular.woff2') format('woff2');
   }
-
   @font-face {
     font-family: 'Poppins';
     font-style: normal;
     font-weight: 600;
-    src: local(''),
-        url('/fonts/poppins-v15-latin-600.woff2') format('woff2');
+    font-display: swap;
+    src: local('Poppins SemiBold'), local('Poppins-SemiBold'),
+        url('/fonts/poppins-v12-latin-600.woff2') format('woff2');
   }
-
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-
     &::before,
     &::after {
       box-sizing: inherit;
     }
   }
-
-  ${({ theme }) => css`
+  ${({ theme, removeBg }) => css`
     html {
       font-size: 62.5%;
     }
-
     body {
       font-family: ${theme.font.family};
       font-size: ${theme.font.sizes.medium};
-      background-color: ${theme.colors.mainBg};
+      ${!removeBg &&
+      css`
+        background-color: ${theme.colors.mainBg};
+      `}
     }
   `}
 `
