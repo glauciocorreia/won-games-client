f2c7584680f472562f9f475c10d5c528ba9094cf
glauciocorreia <glauciocorreiaa@gmail.com>
2021-03-23 22:26:00 -0300

    Home 7 - Métodos de data fetching no NextJs - getStatic, getServerSide

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,36 @@
 import Home from 'templates/Home'
 
-export default function Index() {
-  return <Home />
+export default function Index(props: any) {
+  return <Home {...props} />
+}
+
+// ATENÇÃO
+// os métodos getStatic/getServerSide SÓ FUNCIONAM EM PAGES
+
+// getStaticProps => gerar estático em build time
+// export function getStaticProps() {
+//   // faz lógica
+//   // pode ser buscar dados numa API
+//   // fazer calculo/leitura de context
+
+//   // retorno dos dados de
+//   return {
+//     props: {
+//       heading: 'Olha eu aqui',
+//     },
+//   }
+// }
+
+// getServerSideProps => gerar via ssr a cada request
+export function getServerSideProps() {
+  // faz lógica
+  // pode ser buscar dados numa API
+  // fazer calculo/leitura de context
+
+  // retorno dos dados de
+  return {
+    props: {
+      heading: 'Olha eu aqui 1234 ssr',
+    },
+  }
 }
