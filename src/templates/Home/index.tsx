d0401fe6c3b3c01a7bf844f3fd33de383d8efcc2
glauciocorreia <glauciocorreiaa@gmail.com>
2021-03-23 22:26:38 -0300

    Home 7 - Métodos de data fetching no NextJS - getStatic, getServerSide 2

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -5,7 +5,7 @@ import Heading from 'components/Heading'
 
 import * as S from './styles'
 
-const Home = () => (
+const Home = ({ heading }: any) => (
   <section>
     <Container>
       <Menu />
@@ -19,7 +19,7 @@ const Home = () => (
 
     <Container>
       <Heading lineLeft lineColor="secondary">
-        Most Popular
+        {heading}
       </Heading>
     </Container>
 
