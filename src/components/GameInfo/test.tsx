83a588d1a5d65ea825ae39e4816c792f634a12b9
glauciocorreia <glauciocorreiaa@gmail.com>
2021-04-01 19:19:53 -0300

    GameInfo 2 - Criando estrutura e testes

--- src/components/GameInfo/test.tsx
+++ src/components/GameInfo/test.tsx
@@ -1,15 +1,30 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import GameInfo from '.'
 
+const props = {
+  title: 'My Game Title',
+  description: 'Game Description',
+  price: '210,00',
+}
+
 describe('<GameInfo />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<GameInfo />)
+  it('should render game informations', () => {
+    renderWithTheme(<GameInfo {...props} />)
+
+    expect(screen.getByRole('heading', { name: /my game title/i }))
+    expect(screen.getByText(/game description/i))
+    expect(screen.getByText(/\$210,00/))
+  })
+  it('should render button', () => {
+    renderWithTheme(<GameInfo {...props} />)
 
     expect(
-      screen.getByRole('heading', { name: / GameInfo/i })
+      screen.getByRole('button', { name: /add to cart/i })
     ).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+    expect(
+      screen.getByRole('button', { name: /wishlist/i })
+    ).toBeInTheDocument()
   })
 })
