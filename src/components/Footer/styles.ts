d4b5a6b1637a2e483d8312048d4abb3b54244818
glauciocorreia <glauciocorreiaa@gmail.com>
2021-04-09 14:53:45 -0300

    Criando a Cover Image

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -12,8 +12,8 @@ export const Wrapper = styled.footer`
 export const Content = styled.div`
   ${({ theme }) => css`
     display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid: ${theme.grid.gutter};
+    grid-template-columns: minmax(auto, 50%) 1fr;
+    gap: ${theme.grid.gutter};
     margin-top: ${theme.spacings.medium};
 
     ${media.greaterThan('medium')`
